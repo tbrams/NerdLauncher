57c4fcb4ee5eba670eead2a85b9fb2b358f8d7e0
tbrams <torben@brams.dk>
2015-09-10 12:52:49 +0700

    First commit

---
+++ app/src/main/java/dk/incipio/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,27 @@
+package dk.incipio.nerdlauncher;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
+
+import java.util.List;
+
+public class NerdLauncherFragment extends ListFragment {
+    private static final String TAG = "NerdLauncherFragment";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+
+        Log.i(TAG, "Found "+activities.size()+" activities");
+    }
+}
