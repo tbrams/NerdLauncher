c2cb19ed18f20303e5940ee2f36cacac3592f20f
tbrams <torben@brams.dk>
2015-09-10 13:15:28 +0700

    Readable labels for responding applications added to list in alphabethical order

--- app/src/main/java/dk/incipio/nerdlauncher/NerdLauncherFragment.java
+++ app/src/main/java/dk/incipio/nerdlauncher/NerdLauncherFragment.java
@@ -6,7 +6,13 @@ import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class NerdLauncherFragment extends ListFragment {
@@ -22,6 +28,32 @@ public class NerdLauncherFragment extends ListFragment {
         PackageManager pm = getActivity().getPackageManager();
         List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
 
-        Log.i(TAG, "Found "+activities.size()+" activities");
+        Log.i(TAG, "Found " + activities.size() + " activities");
+
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo lhs, ResolveInfo rhs) {
+                PackageManager pm = getActivity().getPackageManager();
+                return String.CASE_INSENSITIVE_ORDER.compare(lhs.loadLabel(pm).toString(), rhs.loadLabel(pm).toString());
+            }
+        });
+
+        ArrayAdapter<ResolveInfo> adapter = new ArrayAdapter<ResolveInfo>(getActivity(), android.R.layout.simple_list_item_1, activities) {
+            public View getView(int pos, View convertview, ViewGroup parent) {
+                PackageManager pm = getActivity().getPackageManager();
+                View v = super.getView(pos, convertview, parent);
+
+                // Documentation says that simple_list_item_1 is a TextView
+                // Cast it so you can see the text value
+                TextView tv = (TextView)v;
+                ResolveInfo ri = getItem(pos);
+                tv.setText(ri.loadLabel(pm));
+
+                return v;
+            }
+
+        };
+        setListAdapter(adapter);
+
     }
 }
